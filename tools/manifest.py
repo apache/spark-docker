f488d732d254caa78c1e1a2ef74958e6c867dad6
Yikun Jiang <yikunkero@gmail.com>
2022-11-15 21:32:30 +0800

    [SPARK-40519] Add "Publish" workflow to help release apache/spark image
    ### What changes were proposed in this pull request?
    The publish step will include 3 steps:
    1. First build the local image.
    2. Pass related test (K8s test / Standalone test) using image of first step.
    3. After pass all test, will publish to `ghcr` (This might help RC test) or `dockerhub`
    
    It's about 30-40 mins to publish all images.
    
    Add "Publish" workflow to help release apache/spark image.
    ![image](https://user-images.githubusercontent.com/1736354/201015477-30428444-0ed5-4436-8b59-7420c678c4a6.png)
    
    ### Why are the changes needed?
    One click to create the `apche/spark` image.
    
    ### Does this PR introduce _any_ user-facing change?
    No
    
    ### How was this patch tested?
    1. Set default branch in my fork repo
    2. Run workflow manually, https://github.com/Yikun/spark-docker/actions/workflows/publish.yml?query=is%3Asuccess
    
    Closes #23 from Yikun/workflow.
    
    Authored-by: Yikun Jiang <yikunkero@gmail.com>
    Signed-off-by: Yikun Jiang <yikunkero@gmail.com>

---
+++ tools/manifest.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from argparse import ArgumentParser
+import json
+from statistics import mode
+
+
+def parse_opts():
+    parser = ArgumentParser(prog="manifest.py")
+
+    parser.add_argument(
+        dest="mode",
+        choices=["tags"],
+        type=str,
+        help="The print mode of script",
+    )
+
+    parser.add_argument(
+        "-p",
+        "--path",
+        type=str,
+        help="The path to specific dockerfile",
+    )
+
+    parser.add_argument(
+        "-i",
+        "--image",
+        type=str,
+        help="The complete image registry url (such as `apache/spark`)",
+    )
+
+    parser.add_argument(
+        "-f",
+        "--file",
+        type=str,
+        default="versions.json",
+        help="The version json of image meta.",
+    )
+
+    args, unknown = parser.parse_known_args()
+    if unknown:
+        parser.error("Unsupported arguments: %s" % " ".join(unknown))
+    return args
+
+
+def main():
+    opts = parse_opts()
+    filepath = opts.path
+    image = opts.image
+    mode = opts.mode
+    version_file = opts.file
+
+    if mode == "tags":
+        tags = []
+        with open(version_file, "r") as f:
+            versions = json.load(f).get("versions")
+            # Filter the specific dockerfile
+            versions = list(filter(lambda x: x.get("path") == filepath, versions))
+            # Get matched version's tags
+            tags = versions[0]["tags"] if versions else []
+        print(",".join(["%s:%s" % (image, t) for t in tags]))
+
+
+if __name__ == "__main__":
+    main()
