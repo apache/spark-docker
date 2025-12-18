763fbfda79c3bd5e43c68932c6f8bdd8c21d00ab
Dongjoon Hyun <dongjoon@apache.org>
2025-12-17 23:01:59 -0800

    [SPARK-54748] Publish Apache Spark `4.1.0` to docker registry
    ### What changes were proposed in this pull request?
    
    This PR aims to Publish Apache Spark `4.1.0` to docker registry.
    
    ### Why are the changes needed?
    
    Apache Spark 4.1.0 is released officially.
    - https://github.com/apache/spark/releases/tag/v4.1.0
    - https://spark.apache.org/docs/4.1.0/
    - https://dist.apache.org/repos/dist/release/spark/spark-4.1.0/
    
    We need to provide a docker image of Apache Spark `4.1.0`.
    
    ### Does this PR introduce _any_ user-facing change?
    
    No.
    
    ### How was this patch tested?
    
    Pass the CIs.
    
    Closes #99 from dongjoon-hyun/SPARK-54748.
    
    Authored-by: Dongjoon Hyun <dongjoon@apache.org>
    Signed-off-by: Dongjoon Hyun <dongjoon@apache.org>

--- tools/template.py
+++ tools/template.py
@@ -71,7 +71,9 @@ GPG_KEY_DICT = {
     # issuer "gurwls223@apache.org"
     "4.1.0-preview3": "0FE4571297AB84440673665669600C8338F65970",
     # issuer "dongjoon@apache.org"
-    "4.1.0-preview4": "F28C9C925C188C35E345614DEDA00CE834F0FC5C"
+    "4.1.0-preview4": "F28C9C925C188C35E345614DEDA00CE834F0FC5C",
+    # issuer "dongjoon@apache.org"
+    "4.1.0": "F28C9C925C188C35E345614DEDA00CE834F0FC5C"
 }
 
 
